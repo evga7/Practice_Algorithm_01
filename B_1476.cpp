71efc8f379b3983d059774d96bde8367733f7292
evga7 <evga7@naver.com>
2019-08-07 20:07:20 +0900

    백준알고리즘 1476번

---
+++ B_1476.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main() {
+	int E, S, M;
+	int A, B, C;
+	scanf("%d %d %d", &E, &S, &M);
+	A = B = C = 1;
+	int cnt = 1;
+	while (1)
+	{
+		if (A == E && B == S && C == M)
+		{
+			printf("%d", cnt);
+			break;
+		}
+		A %= 15;
+		B %= 28;
+		C %= 19;
+		A++, B++, C++;
+		cnt++;
+	}
+}
