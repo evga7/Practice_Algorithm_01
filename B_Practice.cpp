b3fa2861aebab5bb8a0c427e48ff6064713374f1
evga7 <evga7@naver.com>
2019-08-07 17:54:21 +0900

    1978

---
+++ B_Practice.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+using namespace std;
+int prime[1001];
+int arr[101];
+int main() {
+	int N;
+	int i, j;
+	int cnt = 0;
+	memset(prime, 1, sizeof(prime));
+	prime[1] = 0;
+	for (i = 2; i < 11; i++)
+	{
+		for (j = i * i; j <= i; j++)
+		{
+			if (prime[i])
+			{
+				prime[j] = 0;
+			}
+		}
+	}
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &arr[i]);
+		if (prime[arr[i]])
+			cnt++;
+	}
+	printf("%d", cnt);
+}
