d10f0c8198db2953831916259f79626e5d072d6d
evga7 <evga7@naver.com>
2019-08-10 23:10:18 +0900

    백준알고리즘 15650 N & M (2)

---
+++ B_15650.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+vector<int> v;
+int visited[10];
+int N, M;
+void dfs(int idx,int cnt){
+	int i, j;
+	if (cnt == M)
+	{
+		for (i = 0; i < v.size(); i++)
+		{
+			printf("%d ", v[i]);
+		}
+		printf("\n");
+	}
+	for (i = idx; i <= N; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = 1;
+			v.push_back(i);
+			dfs(i,cnt + 1);
+			visited[i] = 0;
+			v.pop_back();
+		}
+	}
+
+}
+int main()
+{
+ 	scanf("%d %d", &N, &M);
+	dfs(1,0);
+}
