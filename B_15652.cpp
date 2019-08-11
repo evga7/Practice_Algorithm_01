13cd7839b98af79a366ad8a71c1cc6facd85fd49
evga7 <evga7@naver.com>
2019-08-11 12:33:59 +0900

    백준알고리즘 15652번 N과M(4)

---
+++ B_15652.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+vector<int> v;
+int visited[8];
+int N, M;
+void dfs(int idx,int cnt)
+{
+	int i;
+	if (cnt == M)
+	{
+		for (i = 0; i < v.size(); i++)
+			printf("%d ",v[i]);
+		printf("\n");
+	}
+	else
+	{
+		for (i = idx; i <= N; i++)
+		{
+			v.push_back(i);
+			dfs(i,cnt + 1);
+			v.pop_back();
+		}
+	}
+}
+
+int main()
+{
+	scanf("%d %d", &N, &M);
+	dfs(1,0);
+}
