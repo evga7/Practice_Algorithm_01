fd182328891f628b1b9cfa795eb2a9becf75dd8f
evga7 <evga7@naver.com>
2019-08-11 13:26:38 +0900

    백준알고리즘 15655번 N과M(6)

---
+++ B_15655.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+vector<int> v;
+int arr[10];
+int visited[10];
+int N, M;
+void dfs(int idx,int cnt)
+{
+	int i;
+	if (cnt == M)
+	{
+		for (i = 0; i < v.size(); i++)
+			printf("%d ",v[i]);
+		printf("\n");
+	}
+		for (i = idx; i <N; i++)
+		{
+			if (!visited[i])
+			{
+				visited[i] = 1;
+				v.push_back(arr[i]);
+				dfs(i,cnt + 1);
+				v.pop_back();
+				visited[i] = 0;
+			}
+		}
+}
+int main()
+{
+
+	int i;
+	int num;
+	scanf("%d %d", &N, &M);
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	sort(arr, arr + N);
+	dfs(0,0);
+}
