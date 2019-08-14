33ff0ddb3bda45b538168e766dd77cf77259a62d
evga7 <evga7@naver.com>
2019-08-14 23:21:55 +0900

    백준알고리즘 16194번 카드구매하기2

---
+++ B_16194.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int arr[1001];
+int dp[10001];
+int Min(int a, int b)
+{
+	return a > b ? b : a;
+}
+int main()
+{
+	int N;
+	int i,j;
+	scanf("%d", &N);
+	for (i = 1; i <=N; i++)
+		scanf("%d", &arr[i]);
+	dp[1] = arr[1];
+	for (i = 2; i <= N; i++)
+	{
+		dp[i] = arr[i];
+		for (j = 1; j <=i; j++)
+			dp[i] = Min(dp[i-j] + arr[j], dp[i]);
+	}
+	printf("%d", dp[N]);
+}
