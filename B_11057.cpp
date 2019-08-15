451fd89a23a0b0750ee7510dfd6cda4116e6489a
evga7 <evga7@naver.com>
2019-08-15 17:02:30 +0900

    백준알고리즘 11057번 오르막 수

---
+++ B_11057.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+long long dp[1001][1001];
+int main()
+{
+	int i, j,k;
+	for (i = 0; i < 10; i++)
+		dp[1][i] = 1;
+	int N;
+	long long sum = 0;
+	scanf("%d", &N);
+	for (i = 2; i <= N; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = j; k < 10; k++)
+			{
+				dp[i][k] += dp[i-1][j];
+				dp[i][k] %= 10007;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+		sum += dp[N][i];
+	printf("%lld", sum%10007);
+}
+
