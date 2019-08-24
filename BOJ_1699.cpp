467c1fad0e8480e1da6c4d3151103452fe05f404
evga7 <evga7@naver.com>
2019-08-24 18:41:33 +0900

    백준알고리즘 1699번 제곱수의합

---
+++ BOJ_1699.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+#include <queue>
+#include <string.h>
+#include <cmath>
+#include <string>
+using namespace std;
+int N;
+int dp[100001];
+
+int main()
+{
+	int i, j;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++)
+	{
+		for (j = 1; j <=sqrt(i); j++)
+		{
+			if (dp[i] > dp[i - j * j] + 1 || dp[i] == 0)
+			{
+				dp[i] = dp[i - j * j] + 1;
+			}
+		}
+	}
+	printf("%d", dp[N]);
+}
