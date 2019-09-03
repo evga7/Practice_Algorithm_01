f37c196392a14fe1eac4bf5d6eb57bccfb289589
evga7 <evga7@naver.com>
2019-09-03 20:32:10 +0900

    백준알고리즘 2293 동전1

---
+++ BOJ_2293.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int arr[101];
+int dp[10001];
+int main()
+{
+	int N, K;
+	int i,j,k;
+	scanf("%d %d", &N, &K);
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	dp[0] = 1;
+	for (i = 0; i < N; i++) {
+		for (j=arr[i]; j <= K; j++)
+			dp[j] += dp[j - arr[i]];
+	}
+	printf("%d", dp[K]);
+}
