53b2573b1b2a34c519d6b19d242f9a6ae7695727
evga7 <evga7@naver.com>
2019-09-17 20:03:10 +0900

    백준알고리즘 14889번 스타트와 링크

---
+++ BOJ_14889.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int N;
+int arr[21][21];
+int res = 99999999;
+int visited[21];
+void DFS(int cnt, int idx)
+{
+	if (cnt == N / 2)
+	{
+		int i, j;
+		int A, B;
+		A = B = 0;
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+				if (visited[i] && visited[j]) A += arr[i][j];
+				if (!visited[i] && !visited[j])	B += arr[i][j];
+			}
+		}
+		res = min(abs(A - B), res);
+		return;
+	}
+	int i;
+	for (i = idx; i < N; i++)
+	{
+		visited[i] = 1;
+		DFS(cnt + 1, i + 1);
+		visited[i] = 0;
+	}
+}
+int main()
+{
+	int i, j, k;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			scanf("%d", &arr[i][j]);
+	DFS(0, 0);
+	printf("%d", res);
+}
