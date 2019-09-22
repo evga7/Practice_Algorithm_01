89b6bb18ee2d07d555143cfdde089771e1e0ea27
evga7 <evga7@naver.com>
2019-09-22 23:20:04 +0900

    백준알고리즘 2941번 크로아티아 알파벳

---
+++ BOJ_2941.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	vector<string>v = { {"c="}, { "c-" }, { "dz=" }, { "d-" }, { "lj" }, { "nj" }, { "s=" }, { "z=" } };
+	cin >> str;
+	int i;
+	int len = str.size();
+	int cnt = 0;
+	for (i = 0; i < len; i++)
+	{
+		string temp="";
+		string temp2="";
+		if (i + 1 <= len-1)
+		{
+			temp = temp +str[i] + str[i + 1];
+		}
+		if (i + 2 <= len-1)
+		{
+			temp2 = temp2+str[i] + str[i + 1] + str[i + 2];
+		}
+
+		for (auto num : v)
+		{
+			if (!num.compare(temp))
+			{
+				i++;
+				break;
+			}
+			if (!num.compare(temp2))
+			{
+				i += 2;
+				break;
+			}
+		}
+		cnt++;
+	}
+	printf("%d", cnt);
+}
