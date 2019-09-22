b18320425e64fc2418e46e8a46ee3eb518d28179
evga7 <evga7@naver.com>
2019-09-22 23:47:00 +0900

    백준알고리즘 1193번 분수찾기

---
+++ BOJ_1193.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int N;
+	int i = 1;
+	int sum = 0;
+	scanf("%d", &N);
+	while (i + sum < N)
+	{
+		sum += i++;
+	}
+	int a,b;
+	if (i % 2==0)
+		a = N- sum;
+	else 
+		a=i + 1 - (N - sum);
+	b = i+1 - a;
+	printf("%d/%d", a, b);
+}
