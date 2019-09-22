f2bb6c6ec249d37a9267c7059688f4ed60e509a8
evga7 <evga7@naver.com>
2019-09-22 22:57:56 +0900

    백준알고리즘 9012번 괄호

---
+++ BOJ_9012.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#include <stack>
+string str;
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int i;
+	int flag = 0;
+	for (i = 0; i < N; i++)
+	{
+		stack<char>s;
+		cin >> str;
+		flag = 0;
+		for (int j = 0; j < str.size(); j++)
+		{
+			if (str[j] == '(')
+				s.push('(');
+			else
+			{
+				if (!s.empty()&&s.top() == '(')
+					s.pop();
+				else
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+		if (flag||!s.empty())
+			printf("NO\n");
+		else
+			printf("YES\n");
+	}
+
+}
