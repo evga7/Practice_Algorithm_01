24bbdd89cf69e50df49b9c8330a297afad2ee0e4
evga7 <evga7@naver.com>
2019-09-24 20:50:47 +0900

    백준알고리즘 1436번 영화감독 숌

---
+++ BOJ_1436.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int num = 666;
+	int i = 1;
+	int N;
+	string str;
+	scanf("%d", &N);
+	int j;
+	while (i<N)
+	{
+		num++;
+		int flag = 0;
+		int cnt = 0;
+		int temp=num;
+		while (temp > 0)
+		{
+			int s = temp % 10;
+			if (s == 6)
+				cnt++;
+			else
+				cnt = 0;
+			if (cnt == 3)
+			{
+				flag = 1;
+				break;
+			}
+			temp /= 10;
+		}
+		if (flag)
+		{
+			i++;
+			if (i == N)
+			{
+				break;
+			}
+		}
+	}
+	printf("%d", num);
+}
