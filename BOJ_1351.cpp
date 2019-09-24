297765ef4c0134dd45799eb1651953ba924a5891
evga7 <evga7@naver.com>
2019-09-24 20:50:26 +0900

    백준알고리즘 1351번 무한 수열

---
+++ BOJ_1351.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
+map<long long, long long>m;
+long long N, P, Q;
+long long solve(long long N)
+{
+	if (m[N])
+	{
+		return m[N];
+	}
+	else
+		return m[N] = solve(N / P) + solve(N / Q);
+}
+int main()
+{
+	m[0] = 1;
+	scanf("%lld %lld %lld", &N, &P, &Q);
+	solve(N);
+	printf("%lld", m[N]);
+
+}
