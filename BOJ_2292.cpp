852a00a877bbd7aa270a4655b9b9d5b46fca99f2
evga7 <evga7@naver.com>
2019-09-24 20:49:59 +0900

    백준알고리즘 2292번 벌집

---
+++ BOJ_2292.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+queue<char>q;
+stack<char>s;
+int res = 0;
+int main()
+{
+	int N;
+	int i = 1;
+	int cnt = 1;
+	scanf("%d", &N);
+	while (i < N)
+	{
+		i += 6*cnt;
+		cnt++;
+	}
+	printf("%d", cnt);
+}
