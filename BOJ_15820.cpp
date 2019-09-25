9e995fe1317bfa37a76b5c99e97b9f7911977709
evga7 <evga7@naver.com>
2019-09-25 21:44:59 +0900

    백준알고리즘 15820번 맞았는데 왜 틀리죠?

---
+++ BOJ_15820.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+int main()
+{
+	long long  N, M;
+	int i;
+	int flag = 0;
+	long long a, b;
+	scanf("%lld %lld", &N, &M);
+	for (i = 0; i < N; i++)
+	{
+		scanf("%lld %lld", &a, &b);
+		if (a == b)
+			continue;
+		else
+			flag = 1;
+	}
+	if (flag == 1)
+	{
+		printf("Wrong Answer");
+		return 0;
+	}
+	else
+	{
+		flag = 0;
+		for (i = 0; i < M; i++)
+		{
+			scanf("%lld %lld", &a, &b);
+			if (a == b)
+				continue;
+			else
+				flag = 1;
+		}
+	}
+	if (flag == 1)
+		printf("Why Wrong!!!");
+	else
+		printf("Accepted");
+}
