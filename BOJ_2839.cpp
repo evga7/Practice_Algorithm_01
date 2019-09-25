7e5247e4783bb83d55cbbab249c2f796b14f7a6d
evga7 <evga7@naver.com>
2019-09-25 21:25:21 +0900

    백준알고리즘 2839번 설탕 배달

---
+++ BOJ_2839.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+int res = 0;
+int N;
+int main()
+{
+	scanf("%d", &N);
+	while (1)
+	{
+		if (N % 5 == 0)
+		{
+			res += N / 5;
+			break;
+		}
+		if (N<=0)
+		{
+			res = -1;
+			break;
+		}
+		N -= 3;
+		res++;
+	}
+	printf("%d", res);
+}
