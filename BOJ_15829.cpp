e3530e942498f5e654e2821a100f68c10ff16e67
evga7 <evga7@naver.com>
2019-09-25 21:46:45 +0900

    백준알고리즘 15829번 Hashing

---
+++ BOJ_15829.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+string str;
+long long POW(int k)
+{
+	if (k == 0)
+		return 1;
+	int i;
+	long long res = 31;
+	for (i = 2; i <= k; i++)
+	{
+		res= (res*31) % 1234567891;
+	}
+	return res%1234567891;
+}
+int main()
+{
+	long long N;
+	scanf("%lld", &N);
+	long long i;
+	long long sum = 0;
+	cin >> str;
+	for (i = 0; i < N; i++)
+	{
+		long long num = str[i]-'a'+1;
+		long long mul = POW(i);
+		sum += (num * mul) % 1234567891;
+	}
+	printf("%lld", sum%1234567891);
+}
