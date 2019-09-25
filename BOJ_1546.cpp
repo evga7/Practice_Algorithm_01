dbae301e1901c12f9798ce864ac24e07006720a2
evga7 <evga7@naver.com>
2019-09-25 21:26:25 +0900

    백준알고리즘 1546번 평균

---
+++ BOJ_1546.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+double res = 0;
+double N;
+double arr[1001];
+int main()
+{
+	scanf("%lf", &N);
+	int i;
+	double MAX = 0;
+	for (i = 0; i < N; i++)
+	{
+		scanf("%lf", &arr[i]);
+		MAX = max(MAX, arr[i]);
+	}
+	for (i = 0; i < N; i++)
+	{
+		arr[i] = (arr[i] / MAX) * 100;
+		res += arr[i];
+	}
+	printf("%lf", res / N);
+
+}
