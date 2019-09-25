332056656b9edbf6ee813b99206210c9c1a88da7
evga7 <evga7@naver.com>
2019-09-25 21:33:39 +0900

    백준알고리즘 1475번 방 번호

---
+++ BOJ_1475.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+int res = 0;
+int num[10];
+int main()
+{
+	string str;
+	cin >> str;
+	int i;
+	for (i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '9')
+			num[6]++;
+		else
+		num[str[i] - '0']++;
+	}
+	if (num[6] % 2 == 0)
+		num[6] /= 2;
+	else
+		num[6] = (num[6] / 2) + 1;
+	for (i = 0; i < 10; i++)
+	{
+		res = max(res, num[i]);
+	}
+	printf("%d", res);
+}
