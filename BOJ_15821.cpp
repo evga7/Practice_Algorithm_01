10c76514d9f4cbf12c03e883a61cec639cb0db93
evga7 <evga7@naver.com>
2019-09-26 22:43:59 +0900

    백준알고리즘 15821번 낚이고 낚아라

---
+++ BOJ_15821.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+using namespace std;
+vector<double>v;
+int main() {
+	int N, M,K;
+	scanf("%d %d", &N,&K);
+	int i,j;
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &M);
+		double Max = 0;
+		for (j = 0; j < M; j++)
+		{
+			int x, y;
+			scanf("%d %d", &x, &y);
+			double dis = sqrt(pow(x, 2) + pow(y, 2));
+			Max = max(dis, Max);
+		}
+		v.push_back(Max);
+	}
+	sort(v.begin(), v.end());
+	printf("%.2lf", pow(v[K-1], 2));
+}
