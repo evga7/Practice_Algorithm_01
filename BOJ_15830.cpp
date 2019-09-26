87c079d0272a77c5e3ee59551f7b63a323eb0e6c
evga7 <evga7@naver.com>
2019-09-26 22:43:41 +0900

    백준알고리즘 15830번 싱크홀

---
+++ BOJ_15830.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	double V, W;
+	int D;
+	scanf("%lf %lf %d", &V, &W, &D);
+	double pos = 0;
+	double op = 1;
+	long long res = 0;
+	while (1)
+	{
+		double t = W / V;
+		pos += 5 * pow(t,2);
+		if (pos >= D)
+			break;
+		V = V*8 / 10;
+		res++;
+	}
+	printf("%lld", res);
+}
