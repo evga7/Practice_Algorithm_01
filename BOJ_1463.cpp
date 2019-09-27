5c69dadb8658152afec0be12ef1bb32b891a9cb6
evga7 <evga7@naver.com>
2019-09-27 19:53:46 +0900

    백준알고리즘 1463번 1로만들기

---
+++ BOJ_1463.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+int N;
+int dp[100001];
+int main()
+{
+	int N;
+	int i;
+	scanf("%d", &N);
+	for (i = 2; i <=N; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0) dp[i] = min(dp[i / 3] + 1, dp[i]);
+		if (i % 2 == 0) dp[i] = min(dp[i / 2] + 1, dp[i]);
+	}
+	printf("%d", dp[N]);
+}
