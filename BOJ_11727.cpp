187729a055d9f8eef969cd3b1db580eba4c28593
evga7 <evga7@naver.com>
2019-09-27 19:38:02 +0900

    백준알고리즘 11727번 2*n 타일링2

---
+++ BOJ_11727.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+int N;
+int dp[1001];
+int main()
+{
+	dp[0] = 1;
+	dp[1] = 1;
+	int i;
+	scanf("%d", &N);
+	for (i = 2; i <= N; i++)
+		dp[i] = (dp[i - 1] + dp[i - 2]*2) % 10007;
+	printf("%d", dp[N]);
+
+}
