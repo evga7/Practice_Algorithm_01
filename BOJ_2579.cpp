c1893a1ef0c14ebf2c2fd4c7ae4a9f43905afe99
evga7 <evga7@naver.com>
2019-09-27 20:09:36 +0900

    백준알고리즘 2579번 계단 오르기

---
+++ BOJ_2579.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+int N;
+int dp[301];
+int arr[301];
+int main()
+{
+	int N;
+	int i;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	dp[0] = arr[0];
+	for (i = 1; i < N; i++)
+	{
+		dp[i] = max(dp[i-2]+arr[i],arr[i-1]+arr[i]+dp[i-3]);
+	}
+	printf("%d", dp[N-1]);
+}
