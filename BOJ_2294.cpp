bbbd49a198fc04ae5c026be77045f5f6363e2653
evga7 <evga7@naver.com>
2019-09-27 19:25:05 +0900

    백준알고리즘 2294번 동전2

---
+++ BOJ_2294.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+int arr[101];
+int dp[10001];
+int main()
+{
+	int N, K;
+	int i, j, k;
+	scanf("%d %d", &N, &K);
+	for (i = 1; i <=N; i++)
+		scanf("%d", &arr[i]);
+	for (i = 1; i <= K; i++)
+		dp[i] = 999999;
+	dp[0] = 0;
+	for (i=1;i<=N;i++)
+	{
+		for (j = arr[i]; j <= K; j++)
+		{
+			dp[j] = min(dp[j], dp[j - arr[i]] + 1);
+		}
+	}
+	if (dp[K] = 999999)
+		printf("-1");
+	else
+	printf("%d", dp[K]);
+}
