179cece692c14457236f3ee31be1d5b72cc41f2b
evga7 <evga7@naver.com>
2019-09-27 18:43:16 +0900

    백준알고리즘 17127번 벚꽃이 정보섬에 피어난 이유

---
+++ BOJ_17127.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include <algorithm>
+#include<vector>
+using namespace std;
+int arr[11];
+int res = 0;
+int main() {
+	int N;
+	scanf("%d", &N);
+	int i, j;
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	int M = N - 3;
+	int sum;
+	int mul = 1;
+	for (i = 0; i < N; i++)
+	{
+		sum = 0;
+		mul = 1;
+		if (i + M <=N)
+		{
+			for (j = 0; j < N; j++)
+			{
+				if (j >= i && j < i + M)
+					mul *= arr[j];
+				else
+					sum += arr[j];
+			}
+			sum += mul;
+			res = max(sum, res);
+		}
+	}
+	printf("%d", res);
+}
