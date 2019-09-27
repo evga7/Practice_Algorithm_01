1969521578cbb4aed5688a4825c7f0ce765c373b
evga7 <evga7@naver.com>
2019-09-27 18:09:45 +0900

    백준알고리즘 2164번 카드2

---
+++ BOJ_2164.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+queue<int>q;
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int i;
+	for (i = 1; i<=N; i++)
+	{
+		q.push(i);
+	}
+	while (q.size() > 1)
+	{
+		q.pop();
+		int num = q.front();
+		q.pop();
+		q.push(num);
+	}
+	printf("%d", q.front());
+
+}
