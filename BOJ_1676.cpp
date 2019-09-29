826b91fc15e470290874dee70fe9ff9571d6fda5
evga7 <evga7@naver.com>
2019-09-29 16:50:50 +0900

    백준알고리즘 1676번 팩토리얼 0의 개수

---
+++ BOJ_1676.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int N;
+	int res=0;
+	scanf("%d", &N);
+	int i;
+	for (i = 2; i <= N; i++)
+	{
+		if (i%5==0) res++;
+        if (i%25==0)res++;
+        if (i%125==0)res++;
+	}
+    printf("%d",res);
+}
