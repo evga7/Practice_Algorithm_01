01474044a266ed2c07e0ad9df2b57468da6f123e
evga7 <evga7@naver.com>
2019-09-29 17:47:31 +0900

    백준알고리즘 4307번 개미

---
+++ BOJ_4307.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int T;
+	int len, N;
+	int i;
+	int res1, res2;
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%d %d", &len, &N);
+		res1 = 0, res2 = 0;
+		for (i = 0; i < N; i++)
+		{
+			int num;
+			scanf("%d", &num);
+			int MAX = max(num, len - num);
+			int MIN = min(num, len - num);
+			res1 = max(MIN, res1);
+			res2 = max(MAX, res2);
+		}
+		printf("%d %d\n", res1, res2);
+	}
+}
