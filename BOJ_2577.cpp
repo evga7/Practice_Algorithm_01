88d8e608a2961b22ac1982c518ba35e068f06bdd
evga7 <evga7@naver.com>
2019-10-19 22:15:09 +0900

    백준알고리즘 2577번 숫자의 개수

---
+++ BOJ_2577.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+void num()
+{
+	int A, B, C;
+	int cnt[10] = { 0 };
+	int sum;
+	char temp[100];
+	int i;
+	scanf("%d %d %d", &A, &B, &C);
+	sum = A * B*C;
+	sprintf(temp, "%d",sum);
+	for (i = 0;i < strlen(temp);i++)
+		cnt[temp[i]-'0']++;
+	for (i=0;i<10;i++)
+		printf("%d\n",cnt[i]);
+}
+int main()
+{
+	num();
+}
