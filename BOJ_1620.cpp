01f5aded9844e376906c9029e0bb48fb929c7600
evga7 <evga7@naver.com>
2019-10-21 22:48:16 +0900

    백준알고리즘 1620번 나는야 포켓몬 마스터 이다솜

---
+++ BOJ_1620.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <map>
+using namespace std;
+map<string, int> set1;
+map<int, string>set2;
+void pokemon()
+{
+	int N, M;
+	int i,j;
+	int num;
+	scanf("%d %d", &N,&M);
+	char poke_name[21];
+	char op[21];
+	for (i = 0; i < N; i++)
+	{
+		scanf("%s", poke_name);
+		set1[poke_name] = i;
+		set2[i] = poke_name;
+	}
+	for (i = 0; i < M; i++)
+	{
+		scanf("%s", op);
+		if (op[0] >= '0'&&op[0] <= '9')
+		{
+			printf("%s\n", set2[atoi(op)-1].c_str());
+		}
+		else
+			printf("%d\n", set1[op]+1);
+	}
+}
+int main()
+{
+	pokemon();
+}
