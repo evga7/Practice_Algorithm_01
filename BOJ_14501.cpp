2c0a2954bfca4eac126c71615555edf78a94d1c3
evga7 <evga7@naver.com>
2019-10-24 22:34:06 +0900

    백준알고리즘 14501번 퇴사

---
+++ BOJ_14501.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+using namespace std;
+int N;
+int day[20];
+int point[20];
+int dp[20];
+int main()
+{
+	scanf("%d", &N);
+	int i,j;
+	for (i = 1; i <=N; i++)
+		scanf("%d %d", &day[i],&point[i]);
+	for (i = 1; i <= N; i++)
+	{
+		for (j = 1; j<=i; j++)
+		{
+			if (day[j] + i <= N + 1 && dp[day[j] + i] < dp[j] + point[j])
+			{
+				dp[day[j] + i] = dp[j] + point[j];
+			}
+		}
+	}
+	cout << dp[N + 1];
+}
