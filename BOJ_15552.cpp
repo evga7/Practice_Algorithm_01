2a8c407c0d1062ab69f6457f1a44f0862fea72e9
evga7 <evga7@naver.com>
2019-10-26 19:28:55 +0900

    백준알고리즘 15552번 빠른 A+B

---
+++ BOJ_15552.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+void fast_sum()
+{
+	int N;
+	int i;
+	int A, B;
+	scanf("%d", &N);
+	for (i = 0;i < N;i++)
+	{
+		scanf("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}
+}
+int main()
+{
+	fast_sum();
+}
