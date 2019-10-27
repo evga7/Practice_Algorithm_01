45c2c454e897dc46996d097067124fdda5c59e92
evga7 <evga7@naver.com>
2019-10-27 17:25:57 +0900

    백준알고리즘 9372번 상근이의 여행

---
+++ BOJ_9372.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int T,N,M;
+	int a, b;
+	int i;
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%d %d", &N, &M);
+		for (i = 0; i < M; i++)
+		{
+			scanf("%d %d", &a, &b);
+		}
+		printf("%d\n",N - 1);
+	}
+}
