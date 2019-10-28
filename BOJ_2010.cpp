13097a2e6086728ab1c4d444ffd8ed146c6a8e86
evga7 <evga7@naver.com>
2019-10-28 22:36:49 +0900

    백준알고리즘 2010번 플러그

---
+++ BOJ_2010.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+void plug(int N)
+{
+	int i;
+	int num;
+	int cnt;
+	scanf("%d", &num);
+	cnt = num;
+	for (i = 1; i < N; i++)
+	{
+		scanf("%d", &num);
+		cnt += num-1;
+	}
+	printf("%d", cnt);
+}
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	plug(N);
+}
