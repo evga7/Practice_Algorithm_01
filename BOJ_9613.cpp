1c202906cf23e44fb0ba9ed4d4a8b5089df4becf
evga7 <evga7@naver.com>
2019-11-01 22:56:42 +0900

    백준알고리즘 9613번 GCD합

---
+++ BOJ_9613.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+int arr[101];
+int gcd(int a, int b)
+{
+	int temp;
+	while (b != 0)
+	{
+		temp = a % b;
+		a = b;
+		b = temp;
+	}
+	return a;
+}
+int main(){
+	int N, M;
+	int i,j,k,l;
+	long long sum;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++)
+	{
+		sum = 0;
+		scanf("%d", &M);
+		for (j = 1; j <= M; j++)
+			scanf("%d", &arr[j]);
+
+		for (k = 1; k <M; k++)
+		{
+			for (l = k + 1; l<= M;l++)
+			{
+				sum += gcd(arr[k], arr[l]);
+			}
+		}
+		printf("%lld\n", sum);
+	}
+	
+}
