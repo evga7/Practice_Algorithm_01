4b72667fdc7ba2463c21096a240daa323ed816da
evga7 <evga7@naver.com>
2019-11-07 22:30:05 +0900

    백준알고리즘 10808번 알파벳 갯수

---
+++ BOJ_10808.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+void num_of_a()
+{
+	char arr[101];
+	int i;
+	int cnt[26] = { 0 };
+	scanf("%s", arr);
+	for (i = 0;i < strlen(arr);i++)
+	{
+		cnt[arr[i] - 'a']++;
+	}
+	for (i=0;i<26;i++)
+	printf("%d ", cnt[i]);
+}
+int main()
+{
+	num_of_a();
+}
