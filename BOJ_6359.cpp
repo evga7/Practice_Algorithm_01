8aa22c292853b1bb703fce10e310e4542e7a6e41
evga7 <evga7@naver.com>
2019-11-11 23:22:43 +0900

    백준알고리즘 6359번 만취한 상범

---
+++ BOJ_6359.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	int N;
+	while (T--)
+	{
+		int dp[101] = { 0 };
+		scanf("%d", &N);
+		int i,j;
+		for (i = 2; i <= N; i++)
+		{
+			for (j = i; j <=N; j+=i)
+			{
+				dp[j] = dp[j]==0 ? 1 : 0;
+			}
+		}
+		int cnt = 0;
+		for (i = 1; i <= N; i++)
+			if (dp[i] == 0)
+				cnt++;
+		cout << cnt<<endl;
+	}
+}
