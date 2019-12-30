1a534327a1829f0506672106e7f7bbdbf603b958
evga7 <evga7@naver.com>
2019-12-30 22:26:08 +0900

    백준알고리즘 1251번 단어 나누기

---
+++ BOJ_1251.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+using namespace std;
+int len;
+string res;
+void go(string str, int Start, int Second)
+{
+	string A, B, C;
+	int i;
+	A = B = C = "";
+	for (i = 0; i <= Start; i++)
+	{
+		A = A + str[i];
+	}
+	for (i = Start + 1; i <= Second; i++)
+	{
+		B = B + str[i];
+	}
+	for (i = Second + 1; i < len; i++)
+	{
+		C = C + str[i];
+	}
+	reverse(A.begin(), A.end());
+	reverse(B.begin(), B.end());
+	reverse(C.begin(), C.end());
+	string D = "";
+	D = A + B + C;
+	if (res == "")
+		res = D;
+	else
+	{
+		if (res > D)
+			res = D;
+	}
+
+}
+int main()
+{
+	string str;
+	cin >> str;
+	int i, j;
+	res = "";
+	len = str.size();
+	for (i = 0; i < len - 1; i++)
+	{
+		for (j = i + 1; j < len - 1; j++)
+		{
+			if (i >= j)
+				continue;
+			string temp = str;
+			go(temp, i, j);
+		}
+	}
+	cout << res;
+}
