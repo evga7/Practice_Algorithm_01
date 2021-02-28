0d66e8861ac7ec4b804c4ff232ddc4925779ae39
evga7 <evga7@naver.com>
2021-02-28 23:50:22 +0900

    백준알고리즘 2217번 로프

---
+++ BOJ_2217_2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define MAX 987654321
+#define MIN -987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<int, string> pi3;
+typedef pair<int, pair<int, int>>pi2;
+typedef vector<vector<int>> vpi;
+typedef vector<int> vec;
+typedef vector<pi> vec2;
+typedef queue<pi> que;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int N, M;
+int num;
+int res;
+int K;
+vec v;
+bool is_inside(int x, int y)
+{
+	if (x >= N || x < 0 || y < 0 || y >= M)return false;
+	return true;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int i, j;
+	cin >> N;
+	for (i = 0; i < N; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	for (i = N-1; i >=0; i--)
+	{
+		res = max(res, (v[i] * (N-i)));
+	}
+	cout << res;
+}
