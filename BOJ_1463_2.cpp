5c1b14a63e231842ef0fd86b567c61d2eb49e156
evga7 <evga7@naver.com>
2021-02-28 23:49:00 +0900

    백준알고리즘 1463번 1로만들기

---
+++ BOJ_1463_2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define MAX 987654321
+#define MIN -987654321
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<int, string> pi3;
+typedef pair<int, pair<int, int>>pi2;
+typedef pair<int, char> pi4;
+typedef vector<vector<int>> vpi;
+typedef vector<int> vec;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int N, M;
+int num;
+int res;
+int dp[10000001];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int i, j;
+	cin >> N;
+	
+	for (i=2;i<=N;i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+	}
+	cout << dp[N];
+}
